db92de0abce1dbe05f085a2f4d2afc9469dd1056
Roman <kowaliszyn@gazeta.pl>
2016-06-04 15:42:11 +0200

    Correct the assembly and namespace name

--- src/Igorary.Uwp/Properties/AssemblyInfo.cs
+++ src/Igorary.Uwp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Igorary.UWP")]
+[assembly: AssemblyTitle("Igorary.Uwp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Igorary.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Igorary.Uwp")]
+[assembly: AssemblyCopyright("Copyright © 2016 Covalition")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
