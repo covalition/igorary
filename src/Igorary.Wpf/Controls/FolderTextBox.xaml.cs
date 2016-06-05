b756e186e651c61488ca7899423ca97ff4b613b1
Roman <kowaliszyn@gazeta.pl>
2016-06-05 10:57:20 +0200

    Remember the last selected folder

--- src/Igorary.Wpf/Controls/FolderTextBox.xaml.cs
+++ src/Igorary.Wpf/Controls/FolderTextBox.xaml.cs
@@ -26,9 +26,19 @@ namespace Igorary.Xaml.Controls
 
         private void btnSelectFolder_Click(object sender, RoutedEventArgs e) {
             System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            string startFolderPath = FolderPath;
+            if (string.IsNullOrEmpty(startFolderPath))
+                startFolderPath = StartFolderPath;
+            if (string.IsNullOrEmpty(startFolderPath) && RememberLastFolder)
+                startFolderPath = Properties.Settings.Default.LastSelectedFolder;
+            dialog.SelectedPath = startFolderPath;
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
                 tbFolderPath.Text = dialog.SelectedPath;
                 BindingOperations.GetBindingExpression(tbFolderPath, TextBox.TextProperty).UpdateSource();
+                if (RememberLastFolder) {
+                    Properties.Settings.Default.LastSelectedFolder = dialog.SelectedPath;
+                    Properties.Settings.Default.Save();
+                }
             }
         }
 
@@ -40,6 +50,24 @@ namespace Igorary.Xaml.Controls
         // Using a DependencyProperty as the backing store for FolderPath.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty FolderPathProperty =
             DependencyProperty.Register("FolderPath", typeof(string), typeof(FolderTextBox), new PropertyMetadata(null));
+
+        public string StartFolderPath {
+            get { return (string)GetValue(StartFolderPathProperty); }
+            set { SetValue(StartFolderPathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StartFolderPath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StartFolderPathProperty =
+            DependencyProperty.Register("StartFolderPath", typeof(string), typeof(FolderTextBox), new PropertyMetadata(null));
+
+        public bool RememberLastFolder {
+            get { return (bool)GetValue(RememberLastFolderProperty); }
+            set { SetValue(RememberLastFolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RememberLastFolder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RememberLastFolderProperty =
+            DependencyProperty.Register("RememberLastFolder", typeof(bool), typeof(FolderTextBox), new PropertyMetadata(true));
 
     }
 }
