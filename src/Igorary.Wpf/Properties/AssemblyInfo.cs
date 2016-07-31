7e186104a7325b794dbc3b6f7a71359011fa10a2
Roman <kowaliszyn@gazeta.pl>
2016-07-31 14:46:22 +0200

    Change the assembly info

--- src/Igorary.Wpf/Properties/AssemblyInfo.cs
+++ src/Igorary.Wpf/Properties/AssemblyInfo.cs
@@ -8,9 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Igorary.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Igorary.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +20,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e244578f-c42e-40b0-add0-73a5812f4aa1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
