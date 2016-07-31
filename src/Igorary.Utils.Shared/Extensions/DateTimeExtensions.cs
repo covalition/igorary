7ddb4eee3c316837bb56af934328d5bd9014016c
Roman <kowaliszyn@gazeta.pl>
2016-07-31 10:56:43 +0200

    Create the Igorary.Utils.Shared project

---
+++ src/Igorary.Utils.Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igorary.Utils.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime Max(this DateTime source, DateTime dateTime) {
+            return source > dateTime ? source : dateTime;
+        }
+
+        public static DateTime Min(this DateTime source, DateTime dateTime) {
+            if (source == default(DateTime))
+                return dateTime;
+            if (dateTime == default(DateTime))
+                return source;
+            return source < dateTime ? source : dateTime;
+        }
+    }
+}
