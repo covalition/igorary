0b8149f9a67ae7af12bd85569999f8328d1b1365
Roman <kowaliszyn@gazeta.pl>
2016-12-17 18:18:46 +0100

    Create Igorary.EntityFramework

---
+++ src/Igorary.EntityFramework/IDbContextCommon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Covalition.Igorary.EntityFramework
+{
+    public interface IDbContextCommon: IDisposable
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        int SaveChanges();
+
+        /// <summary>
+        /// Updates relations between the objects (i.e. two-way navigation)
+        /// </summary>
+        void UpdateObjectGraph();
+
+        DbContextTransaction BeginTransaction();
+    }
+}
