c21bb74ba3e03329fe5ede49ef2e0d290d7cca8b
Roman <kowaliszyn@gazeta.pl>
2017-01-01 16:58:04 +0100

    Create Igorary.Mvc

---
+++ src/Igorary.Mvc/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Covalition.Igorary.Mvc.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItems(this IDictionary<int, string> lookupData) {
+            return lookupData.Select(di => new SelectListItem { Value = di.Key.ToString(), Text = di.Value });
+        }
+    }
+}
