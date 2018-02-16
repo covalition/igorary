629d5094ec64b16eaffb56901ac4c16d0b8d9ddc
Roman <kowaliszyn@gazeta.pl>
2018-02-17 00:40:22 +0100

    ICollection.Register

---
+++ src/Igorary.Utils.Shared/Extensions/CollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Igorary.Utils.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static T Register<T>(this ICollection<T> collection, T obj) {
+            collection.Add(obj);
+            return obj;
+        }
+    }
+}
