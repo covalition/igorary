ec18ea1ad86b7ca5fce1495a393396b5aca86b69
Roman <kowaliszyn@gazeta.pl>
2019-06-03 20:56:43 +0200

    Add SortAttribute

---
+++ src/Igorary.Blazor/Attributes/SortAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covalition.Igorary.Blazor.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SortAttribute: Attribute
+    {
+        public string SortId { get; set; }
+    }
+}
