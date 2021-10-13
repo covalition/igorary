1c43c27f500c8b66362f6af975707ac31c9dc326
Roman <kowaliszyn@gazeta.pl>
2021-10-13 17:55:27 +0200

    Bug fix in EnumExtensions.GetDisplayName

--- src/Igorary.Utils.Shared/Extensions/EnumExtensions.cs
+++ src/Igorary.Utils.Shared/Extensions/EnumExtensions.cs
@@ -1,24 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace Igorary.Utils.Extensions
 {
     public static class EnumExtensions
     {
         public static string GetDisplayName(this Enum enumValue)
-        { 
-            return enumValue.GetType()
+        {
+            string res = enumValue.GetType()
                 .GetMember(enumValue.ToString())
-                .First()
-                .GetCustomAttribute<DisplayAttribute>()
+                .FirstOrDefault()?
+                .GetCustomAttribute<DisplayAttribute>()?
                 .GetName();
+
+            return res ?? enumValue.ToString();
         }
 
-        public static bool In<T>(this T enumValue, params T[] enumSet) where T: Enum {
+        public static bool In<T>(this T enumValue, params T[] enumSet) where T : Enum
+        {
             return enumSet.Contains(enumValue);
         }
     }
