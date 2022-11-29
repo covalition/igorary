6a090255be53d63b9b6f7444ce8070d3023fa7b8
Roman <kowaliszyn@gazeta.pl>
2022-11-29 17:28:41 +0100

    Func bool extenstions

---
+++ src/Igorary.Utils.Shared/Extensions/FuncBoolExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Igorary.Utils.Extensions
+{
+    public static class FuncBoolExtensions
+    {
+        public static async Task WaitForIt(this Func<bool> condition, int frequency = 25, int milisecondsTimeout = -1, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var waitTask = Task.Run(async () =>
+            {
+                while (!condition())
+                    await Task.Delay(frequency, cancellationToken);
+            }, cancellationToken);
+
+            if (waitTask != await Task.WhenAny(waitTask, Task.Delay(milisecondsTimeout, cancellationToken)))
+                throw new TimeoutException();
+        }
+    }
+}
